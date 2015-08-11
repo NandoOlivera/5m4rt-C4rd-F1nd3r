2f41abeb33a3abf34afe9df01ca07faccbc5723a
NandoOlivera <fernando.olivera.casas@gmail.com>
2015-08-11 18:46:25 -0300

    Dia submit

--- assets/website/index.php
+++ assets/website/index.php
@@ -12,7 +12,7 @@
 <meta property="og:image" content="img/share.png?a=1" />
 
 <title>Magic Card Reveal</title>
-<link rel="shortcut icon" href="favicon.ico" />
+<link rel="shortcut icon" href="img/favicon.ico" />
 <link rel="stylesheet" type="text/css" href="css/estilos.css"/>
 <script src="js/libs/jquery-1.8.3.min.js"></script>
 <script src="js/com/Main.js"></script>
@@ -26,7 +26,7 @@
 		<div id="holder_video">
 			<iframe width="100%" height="100%" src="https://www.youtube.com/embed/JeKvv1szuh8?autoplay=1&rel=0" frameborder="0" allowfullscreen></iframe>
 		</div>
-		<div id="support_btn">Support</div>
+		<a href="mailto:support@magiccardreveal.com"><div id="support_btn">Support</div></a>
 		<img id="app_store_btn" src="img/app_store_btn.png"/>
 		
 	</div>
